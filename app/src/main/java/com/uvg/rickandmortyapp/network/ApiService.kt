cc8eb57b9f56b440891acebebf747f71519b40da
Alexistodj124 <alexistodj@gmail.com>
2025-01-30 14:55:36 -0600

    Location Model and API

--- app/src/main/java/com/uvg/rickandmortyapp/network/ApiService.kt
+++ app/src/main/java/com/uvg/rickandmortyapp/network/ApiService.kt
@@ -2,6 +2,7 @@ package com.uvg.rickandmortyapp.network
 
 import com.uvg.rickandmortyapp.model.Character
 import com.uvg.rickandmortyapp.model.Episode
+import com.uvg.rickandmortyapp.model.Location
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -12,10 +13,13 @@ interface ApiService {
     suspend fun getCharacters(@Query("page") page: Int = 1): CharacterResponse
     @GET("episode")
     suspend fun getEpisodes(@Query("page") page: Int = 1): EpisodeResponse
+    @GET("location")
+    suspend fun getLocations(@Query("page") page: Int = 1): LocationResponse
 }
 
 data class CharacterResponse(val results: List<Character>)
 data class EpisodeResponse(val results: List<Episode>)
+data class LocationResponse(val results: List<Location>)
 
 // Configuración de Retrofit
 object RetrofitInstance {
