7c0be4e545e71a2d200c91103070ceeb8e9df00b
Alexistodj124 <alexistodj@gmail.com>
2025-01-31 11:42:00 -0600

    residents

--- app/src/main/java/com/uvg/rickandmortyapp/model/Location.kt
+++ app/src/main/java/com/uvg/rickandmortyapp/model/Location.kt
@@ -1,9 +1,29 @@
 package com.uvg.rickandmortyapp.model
 
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.annotations.JsonAdapter
+import com.google.gson.annotations.SerializedName
+import com.google.gson.*
+import java.lang.reflect.Type
+
 data class Location(
     val id: Int,
     val name: String,
     val type: String,
     val dimension: String,
-    val residents: List<String>
+
+    @JsonAdapter(StringListJsonAdapter::class)
+    @SerializedName("residents") val residents: List<String>
 )
+
+class StringListJsonAdapter : JsonDeserializer<List<String>> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): List<String> {
+        return when {
+            json.isJsonArray -> json.asJsonArray.mapNotNull { it.asString } // ✅ Si es un array, lo convertimos en una lista
+            json.isJsonPrimitive -> listOf(json.asString) // ✅ Si es un solo String, lo convertimos en una lista de un solo elemento
+            else -> emptyList()
+        }
+    }
+}
--- app/src/main/java/com/uvg/rickandmortyapp/viewmodel/LocationViewModel.kt
+++ app/src/main/java/com/uvg/rickandmortyapp/viewmodel/LocationViewModel.kt
@@ -26,4 +26,6 @@ class LocationViewModel : ViewModel() {
             }
         }
     }
+
 }
+
